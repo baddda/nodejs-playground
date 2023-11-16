31e70cc063772eea23503eb53ef8aaa7a051e964
Alexej Hamburg <alexej@simpletechs.net>
2023-11-16 22:16:58 +0100

    Remove front-end files.

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const appController = require('../controllers/appController.js')
-
-// Application Routes
-router.get('/', appController.home)
-router.get('/dependencies', appController.dependencies)
-router.get('/minimumSecure', appController.minimumSecurePage)
-router.get('/latestReleases', appController.latestReleasesPage)
+const express = require("express");
+const router = express.Router();
+const appController = require("../controllers/appController.js");
 
 // API Routes
-router.get('/api/minimum-secure', appController.minimumSecure)
-router.get('/api/latest-releases', appController.latestReleases)
+router.get("/api/minimum-secure", appController.minimumSecure);
+router.get("/api/latest-releases", appController.latestReleases);
 
-module.exports = router
+module.exports = router;
